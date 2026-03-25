378d5683625401195c8b1345718eeb40ee0bee83
Unaire123 <unair163@gmail.com>
2026-03-25 09:16:37 +0100

    JavaDoc-a saiakera

---
+++ src/javaDoc-ak/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"main","c":"KalitateTxostena","l":"KalitateTxostena()","u":"%3Cinit%3E()"},{"p":"main","c":"KalitateTxostena","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
